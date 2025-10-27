e1e6fd2353831e1e6002d61301b394129f9fb104
ajayygurjar <ajay1651@gmail.com>
2025-10-27 12:41:34 +0530

    Unknown route- Wildcard Route

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express=require('express');
-let port=3000;
+let port=4000;
 const app=express();
 app.use(express.json())
-app.get('/orders',(req,res)=>{
-    res.send('Here is the list of all orders')
+app.get('/products',(req,res)=>{
+    res.send('Here is the list of all products')
     
 })
 
-app.post('/orders',(req,res)=>{
-    res.send('A new order has been created')
+app.post('/products',(req,res)=>{
+    res.send('A new product has been added')
     
 })
 
-app.get('/users',(req,res)=>{
-    res.send('Here is the list of all users')
+app.get('/categories',(req,res)=>{
+    res.send('Here is the list of all categories')
     
 })
 
-app.post('/users',(req,res)=>{
-    res.send('A new user has been created')
+app.post('/categories',(req,res)=>{
+    res.send('A new category has been created')
+})
+
+// Wildcard route - Handle all undefined routes (404 error)
+app.use((req,res)=>{
+    res.status(404).send(`<h1>404 - Page Not Found </h1>`)
 })
 
 
