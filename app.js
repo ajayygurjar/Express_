c1d3f86764994218e885b065c84b74a7a65e0fe5
ajayygurjar <ajay1651@gmail.com>
2025-10-27 10:59:08 +0530

    Middleware implementation

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express=require('express');
-
+let port=3000;
 const app=express();
 app.use((req,res,next)=>{
-    console.log(`Server is up and running on port 3000! Ready to handle requests.`)   
+    console.log(`Authentication middleware called`);
+    next();
+})
+app.use('/library-2',(req,res,next)=>{
+    console.log(`Book Recommendations`);
+    next();
 })
 
-app.listen(3000)
+app.use('/library-3',(req,res,next)=>{
+    console.log(`Special access to research paper from professors and seniors`)
+    next();
+})
+
+app.get('/library-2',(req,res)=>{
+    res.send('<h1>Library 2 Entered</h1>')
+})
+
+app.get('/library-3',(req,res)=>{
+    res.send('<h1>Library 3 Entered</h1>')
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is running`)
+})
