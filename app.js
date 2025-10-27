069f1b09017fab8ff3eb9a4626796636a50ade53
ajayygurjar <ajay1651@gmail.com>
2025-10-27 12:30:52 +0530

    Routes_Postman_testing

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express=require('express');
 let port=3000;
 const app=express();
-app.use((req,res,next)=>{
-    console.log(`Authentication middleware called`);
-    next();
-})
-app.use('/welcome',(req,res,next)=>{
-    req.user='Guest'
-    next();
+app.use(express.json())
+app.get('/orders',(req,res)=>{
+    res.send('Here is the list of all orders')
+    
 })
 
+app.post('/orders',(req,res)=>{
+    res.send('A new order has been created')
+    
+})
 
-app.get('/welcome',(req,res)=>{
-    res.send(`<h1>Welcome, ${req.user}|! </h1>`)
+app.get('/users',(req,res)=>{
+    res.send('Here is the list of all users')
+    
+})
+
+app.post('/users',(req,res)=>{
+    res.send('A new user has been created')
 })
 
 
 app.listen(port,()=>{
     console.log(`Server is running`)
+    
 })
