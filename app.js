a5127dd9c24e758b95f57165e3787f71d47c7e34
ajayygurjar <ajay1651@gmail.com>
2025-10-28 15:11:00 +0530

    Created a Server with Middleware for Logging

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require("express");
 let port = 4000;
 const app = express();
 app.use(express.json());
-app.get("/welcome/:username", (req, res) => {
-  const username = req.params.username;
 
-  const role = req.query.role;
-  res.send(`Welcome ${username}, your role is ${role}`);
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next(); 
+});
+
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products");
+});
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been created");
+});
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created");
 });
 
 app.listen(port, () => {
