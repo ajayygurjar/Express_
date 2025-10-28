e23d57f7559073470138b1ac4891ca342087560b
ajayygurjar <ajay1651@gmail.com>
2025-10-28 15:03:04 +0530

    Dynamic Routes and Query Params

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
-const express=require('express');
-let port=4000;
-const app=express();
-app.use(express.json())
-app.get('/products',(req,res)=>{
-    res.send('Here is the list of all products')
-    
-})
+const express = require("express");
+let port = 4000;
+const app = express();
+app.use(express.json());
+app.get("/welcome/:username", (req, res) => {
+  const username = req.params.username;
 
-app.post('/products',(req,res)=>{
-    res.send('A new product has been added')
-    
-})
+  const role = req.query.role;
+  res.send(`Welcome ${username}, your role is ${role}`);
+});
 
-app.get('/categories',(req,res)=>{
-    res.send('Here is the list of all categories')
-    
-})
-
-app.post('/categories',(req,res)=>{
-    res.send('A new category has been created')
-})
-
-// Wildcard route - Handle all undefined routes (404 error)
-app.use((req,res)=>{
-    res.status(404).send(`<h1>404 - Page Not Found </h1>`)
-})
-
-
-app.listen(port,()=>{
-    console.log(`Server is running`)
-    
-})
+app.listen(port, () => {
+  console.log(`Server is running`);
+});
